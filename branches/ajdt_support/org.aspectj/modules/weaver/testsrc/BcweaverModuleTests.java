dcf21ee326e33782c3b72ca671d4de0d88365ec0
nobody <nobody@49b3fb5d-b372-0410-9e54-86ee76f753a0>
2004-04-26 09:46:30 +0000

    This commit was manufactured by cvs2svn to create branch 'ajdt_support'.

---
+++ branches/ajdt_support/org.aspectj/modules/weaver/testsrc/BcweaverModuleTests.java
@@ -0,0 +1,31 @@
+/* *******************************************************************
+ * Copyright (c) 2002 Palo Alto Research Center, Incorporated (PARC).
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Common Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html 
+ *  
+ * Contributors: 
+ *     PARC     initial implementation 
+ * ******************************************************************/
+
+// default package
+
+import junit.framework.*;
+import junit.framework.Test;
+
+public class BcweaverModuleTests extends TestCase {
+
+    public static Test suite() { 
+        TestSuite suite = new TestSuite(BcweaverModuleTests.class.getName());
+        suite.addTest(org.aspectj.weaver.bcel.BcelTests.suite()); 
+        suite.addTest(org.aspectj.weaver.BcweaverTests.suite()); 
+		suite.addTest(org.aspectj.weaver.patterns.PatternsTests.suite()); 
+        suite.addTestSuite(LocaleTest.class);
+        return suite;
+    }
+
+    public BcweaverModuleTests(String name) { super(name); }
+
+}  
