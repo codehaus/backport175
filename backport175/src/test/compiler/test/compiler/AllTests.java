911a44f50f1e0d0dd49c18bb6bfc1703c035044e
jboner <jboner@49b3fb5d-b372-0410-9e54-86ee76f753a0>
2005-02-09 13:34:01 +0000

    renamed

---
+++ backport175/src/test/compiler/test/compiler/AllTests.java
@@ -0,0 +1,33 @@
+/*******************************************************************************************
+ * Copyright (c) Jonas Bonér, Alexandre Vasseur. All rights reserved.                      *
+ * http://backport175.codehaus.org                                                         *
+ * --------------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of Apache License Version 2.0 *
+ * a copy of which has been included with this distribution in the license.txt file.       *
+ *******************************************************************************************/
+package test.compiler;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import test.compiler.validation.ValidationTest;
+
+/**
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class AllTests extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("All tests");
+
+        suite.addTestSuite(ValidationTest.class);
+
+        return suite;
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+
+}
